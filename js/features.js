86c1210a087c33cf8b952f05ddd23b4877aa21ba
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-09-23 14:13:10 +0600

    History section fully done

--- js/features.js
+++ js/features.js
@@ -6,7 +6,9 @@ function idleBtn(id) {
   id.classList.add('text-[#111111B2]', 'border');
 }
 
-function donated(inp, have) {
+function donated(inp, have, place) {
+  const donationPlace = document.getElementById(place).innerText;
+
   const inputNumber = parseFloat(document.getElementById(inp).value);
   if (isNaN(inputNumber) || inputNumber <= 0) {
     alert('Please enter valid number');
@@ -22,4 +24,12 @@ function donated(inp, have) {
   document.getElementById(inp).value = '';
   const balanceNumber = parseFloat(balance.innerText);
   balance.innerText = balanceNumber - inputNumber;
+  const time = new Date();
+  const history = document.getElementById('history_container');
+  history.innerHTML += `
+  <div class="border rounded-xl p-6">
+        <h1 class="font-bold text-xl"> ${inputNumber} Taka is ${donationPlace}</h1>
+        <p class="text-base font-light text-[#111111B2]"> Date: ${time}</p>
+       </div>
+  `;
 }
--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ historyBtn.addEventListener('click', function () {
   donationPage.classList.remove('text-black', 'primary_btn_bg');
   activeBtn(historyBtn);
   idleBtn(donationPage);
+  document.getElementById('donation_container').classList.add('hidden');
+  document.getElementById('history_container').classList.remove('hidden');
 });
 
 donationPage.addEventListener('click', function () {
@@ -19,20 +21,23 @@ donationPage.addEventListener('click', function () {
   historyBtn.classList.remove('text-black', 'primary_btn_bg');
   activeBtn(donationPage);
   idleBtn(historyBtn);
+  document.getElementById('donation_container').classList.remove('hidden');
+  document.getElementById('history_container').classList.add('hidden');
 });
 
-document.getElementById('donation_btn').addEventListener('click',function(){
-    donated('noakhali-input','noakhali-total')
-})
+document.getElementById('donation_btn').addEventListener('click', function () {
+  donated('noakhali-input', 'noakhali-total', 'donation-noakhali');
+
+});
 
 document
   .getElementById('donation_btn_feni')
   .addEventListener('click', function () {
-    donated('feni-input', 'feni-total');
+    donated('feni-input', 'feni-total', 'donation-feni');
   });
 
 document
   .getElementById('donation_btn_quota')
   .addEventListener('click', function () {
-    donated('quota-input', 'quota-total');
+    donated('quota-input', 'quota-total', 'donation-quota');
   });
