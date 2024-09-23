aeaa354677c796b0ef7a2aa1e472626a42310059
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-09-23 17:01:30 +0600

    Donation page work fully done

--- js/features.js
+++ js/features.js
@@ -8,28 +8,28 @@ function idleBtn(id) {
 
 function donated(inp, have, place) {
   const donationPlace = document.getElementById(place).innerText;
-
   const inputNumber = parseFloat(document.getElementById(inp).value);
-  if (isNaN(inputNumber) || inputNumber <= 0) {
-    alert('Please enter valid number');
-    return;
-  }
+  const balanceNumber = parseFloat(balance.innerText);
   const donationAmountTotal = parseFloat(
     document.getElementById(have).innerText
   );
 
-  const total = donationAmountTotal + inputNumber;
-
-  document.getElementById(have).innerText = total;
-  document.getElementById(inp).value = '';
-  const balanceNumber = parseFloat(balance.innerText);
-  balance.innerText = balanceNumber - inputNumber;
-  const time = new Date();
-  const history = document.getElementById('history_container');
-  history.innerHTML += `
-  <div class="border rounded-xl p-6">
-        <h1 class="font-bold text-xl"> ${inputNumber} Taka is ${donationPlace}</h1>
-        <p class="text-base font-light text-[#111111B2]"> Date: ${time}</p>
-       </div>
-  `;
+  if (isNaN(inputNumber) || inputNumber <= 0 || inputNumber > balanceNumber) {
+    alert('Please enter valid number');
+    return;
+  } else {
+    const time = new Date();
+    const history = document.getElementById('history_container');
+    const total = donationAmountTotal + inputNumber;
+    document.getElementById(have).innerText = total;
+    document.getElementById(inp).value = '';
+    balance.innerText = balanceNumber - inputNumber;
+    document.getElementById('my_modal_5').showModal();
+    history.innerHTML += `
+    <div class="border rounded-xl p-6">
+          <h1 class="font-bold text-xl"> ${inputNumber} Taka is ${donationPlace}</h1>
+          <p class="text-base font-light text-[#111111B2] bg-gray-100 rounded-xl p-4 mt-4"> Date: ${time}</p>
+         </div>
+    `;
+  }
 }
